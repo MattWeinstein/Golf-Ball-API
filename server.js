a21fe209ce2da017f0bb12897cf0ae5d2b46d09f
MattWeinstein <102390205+MattWeinstein@users.noreply.github.com>
2022-06-06 23:49:58 -0400

    first commit

--- server.js
+++ server.js
@@ -5,58 +5,117 @@ const PORT = 8000
 
 app.use(cors())
 
-let rappers = {
-    '21 savage': {
-        'age': 28,
-        'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthdate': '22 October 1992', 
-        'birthLocation': 'London, England',
-        'origin': 'Atlanta, Georgia',
-        'genre': 'hip hop, trap, rap, horrorcore',
-        'occupation': 'rapper, songwriter, record producer',
-        'yearsActive': '2013-present',
-        'labels': 'Epic, Slaughter Gang',
-        'children': 3
+let golfBalls = {
+    "prov1":{
+        "brand":"titleist",
+        "color":"white",
+        "swingSpeed": "medium",
+        "stiffness":"soft",
+        "quality":"great",        
     },
-    'chance the rapper':{
-        'age': 28,
-        'birthName': 'Chancelor Jonathan Bennett',
-        'birthdate': 'April 16, 1993', 
-        'birthLocation': 'London, England',
-        'origin': 'Chicago, Illinois',
-        'genre': 'hip hop, alternative hip hop, r & b',
-        'occupation': 'rapper, singer, song writer, record producer, activist, actor, philanthropist',
-        'yearsActive': '2011-present',
-        'labels': 'none',
-        'children': 0
+    "prov1x":{
+        "brand":"titleist",
+        "color":"white",
+        "swingSpeed": "fast",
+        "stiffness":"soft",
+        "quality":"great",        
     },
-    'unknown':{
-        'age': 'unknown',
-        'birthName': 'unknown',
-        'birthdate': 'unknown', 
-        'birthLocation': 'unknown',
-        'origin': 'unknown',
-        'genre': 'unknown',
-        'occupation': 'unknown',
-        'yearsActive': 'unknown',
-        'labels': 'unknown',
-        'children': 'unknown'
-    }
+    "v300":{
+        "brand":"slazenger",
+        "color":"white",
+        "swingSpeed": "fast",
+        "stiffness":"soft",
+        "quality":"poor",        
+    },
+    "chromesoft":{
+        "brand":"Callaway",
+        "color":"white",
+        "swingSpeed": "fast",
+        "stiffness":"soft",
+        "quality":"great",        
+    },
+    "ball":{
+        "brand":"medium",
+        "color":"white",
+        "swingSpeed": "medium",
+        "stiffness":"medium",
+        "quality":"medium",        
+    },
 }
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:name', (request, response) => {
-    const rapperName = request.params.name.toLowerCase()
-    if(rappers[rapperName]){
-        response.json(rappers[rapperName])
-    }else{
-        response.json(rappers['unknown'])
+app.get('/api/',(req,res)=>{
+    res.json(golfBalls)
+})
+
+app.get('/api/:golfball',(req,res)=>{
+    let golfBall = req.params.golfball.toLowerCase()
+    if(!golfBalls[golfBall]){
+        golfBall='ball'
     }
+    res.json(golfBalls[golfBall])
 })
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(PORT,()=>{
+    console.log('server is running')
 })
+
+// let rappers = {
+//     '21 savage': {
+//         'age': 28,
+//         'birthName': 'Shéyaa Bin Abraham-Joseph',
+//         'birthdate': '22 October 1992', 
+//         'birthLocation': 'London, England',
+//         'origin': 'Atlanta, Georgia',
+//         'genre': 'hip hop, trap, rap, horrorcore',
+//         'occupation': 'rapper, songwriter, record producer',
+//         'yearsActive': '2013-present',
+//         'labels': 'Epic, Slaughter Gang',
+//         'children': 3
+//     },
+//     'chance the rapper':{
+//         'age': 28,
+//         'birthName': 'Chancelor Jonathan Bennett',
+//         'birthdate': 'April 16, 1993', 
+//         'birthLocation': 'London, England',
+//         'origin': 'Chicago, Illinois',
+//         'genre': 'hip hop, alternative hip hop, r & b',
+//         'occupation': 'rapper, singer, song writer, record producer, activist, actor, philanthropist',
+//         'yearsActive': '2011-present',
+//         'labels': 'none',
+//         'children': 0
+//     },
+//     'unknown':{
+//         'age': 'unknown',
+//         'birthName': 'unknown',
+//         'birthdate': 'unknown', 
+//         'birthLocation': 'unknown',
+//         'origin': 'unknown',
+//         'genre': 'unknown',
+//         'occupation': 'unknown',
+//         'yearsActive': 'unknown',
+//         'labels': 'unknown',
+//         'children': 'unknown'
+//     }
+// }
+
+// app.get('/', (request, response) => {
+//     response.sendFile(__dirname + '/index.html')
+// })
+
+// app.get('/api/:name', (request, response) => {
+//     const rapperName = request.params.name.toLowerCase()
+//     if(rappers[rapperName]){
+//         response.json(rappers[rapperName])
+//     }else{
+//         response.json(rappers['unknown'])
+//     }
+// })
+
+// app.listen(process.env.PORT || PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
